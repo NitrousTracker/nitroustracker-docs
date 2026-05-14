d4e77948ad6f19c5295369622e2f0af002d9c7fc
prayerie <prayerie@prayerie.pt>
2026-05-14 19:36:34 +0000

    build.sh: use hugo-geekdoc version compatible with hugo 1.5.2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ ! -d themes/hugo-geekdoc ]; then
 	mkdir -p _deps || true
 	mkdir -p themes/hugo-geekdoc || true
 	if [ ! -f _deps/hugo-geekdoc.tar.gz ]; then
-		wget -O _deps/hugo-geekdoc.tar.gz https://github.com/thegeeklab/hugo-geekdoc/releases/latest/download/hugo-geekdoc.tar.gz
+		wget -O _deps/hugo-geekdoc.tar.gz https://github.com/thegeeklab/hugo-geekdoc/releases/download/v2.0.0/hugo-geekdoc.tar.gz
 	fi
 	tar xzvfC _deps/hugo-geekdoc.tar.gz themes/hugo-geekdoc
 fi
